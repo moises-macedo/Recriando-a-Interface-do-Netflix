a3d1ed49c3004348764b768403a2328288a71e3f
Moisés Alex de Castro Macedo <moises.macedo@hotmail.com.br>
2022-01-02 21:20:20 -0300

    versão 3.0

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 var owl = $('.owl-carousel');
 owl.owlCarousel({
-    items:4,
+    items:5,
     loop:true,
     margin:10,
     autoplay:true,
-    autoplayTimeout:2000,
+    autoplayTimeout:5000,
     autoplayHoverPause:true
+    
 });
 $('.play').on('click',function(){
     owl.trigger('play.owl.autoplay',[2000])
@@ -13,3 +14,4 @@ $('.play').on('click',function(){
 $('.stop').on('click',function(){
     owl.trigger('stop.owl.autoplay')
 })
+
